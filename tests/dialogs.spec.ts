d941875100db3f3c1a264315592e22f5a972fc76
U-Dev <udaythakur.shely@gmail.com>
2026-04-02 14:43:16 -0700

    dialogs

--- tests/dialogs.spec.ts
+++ tests/dialogs.spec.ts
@@ -9,12 +9,72 @@ test("Simple dialog", async ({ page }) => {
     expect(dialog.type()).toBe("alert");
 
     console.log("Dialog message is:", dialog.message());
-    expect(dialog.message()).toBe("I am an alert box");
+    expect(dialog.message()).toBe("I am an alert box!");
 
     dialog.accept();
-
-    await page.locator("#alertBtn").click(); // Opens dialog
-
-    await page.waitForTimeout(5000);
   });
+
+  await page.locator("#alertBtn").click(); // Opens simple dialog
+});
+
+test("confirmation dialog", async ({ page }) => {
+  await page.goto("https://testautomationpractice.blogspot.com/");
+
+  // Register dialog handler
+  page.on("dialog", async (dialog) => {
+    console.log("Dialog type is:", dialog.type());
+    expect(dialog.type()).toBe("confirm");
+
+    console.log("Dialog message is:", dialog.message());
+    expect(dialog.message()).toBe("Press a button!");
+
+    dialog.accept(); // close dialog by accepting
+
+    // dialog.dismiss(); // close dialog by desiminssing
+  });
+
+  await page.locator("#confirmBtn").click(); // Opens confirm dialog
+
+  const validateText: string = await page.locator("#demo").innerText();
+  console.log("Output text is:", validateText);
+  await expect(page.locator("#demo")).toHaveText("You pressed OK!");
+
+  // const validateText: string = await page.locator("#demo").innerText();
+  // console.log("Output text is:", validateText);
+  // await expect(page.locator("#demo")).toHaveText("You pressed Cancel!");
+
+  await page.waitForTimeout(5000);
+});
+
+test.only("prompt  dialog", async ({ page }) => {
+  await page.goto("https://testautomationpractice.blogspot.com/");
+
+  // Register dialog handler
+  page.on("dialog", async (dialog) => {
+    console.log("Dialog type is:", dialog.type());
+    expect(dialog.type()).toBe("prompt");
+
+    console.log("Dialog message is:", dialog.message());
+    expect(dialog.message()).toBe("Please enter your name:");
+
+    expect(dialog.defaultValue()).toContain("Harry Potter");
+
+    dialog.accept("Jerry"); // close dialog by accepting
+
+    // dialog.dismiss(); // close dialog by desiminssing
+  });
+
+  await page.locator("#promptBtn").click(); // Opens prompt dialog
+
+  const validateText: string = await page.locator("#demo").innerText();
+  console.log("Output text is:", validateText);
+  await expect(page.locator("#demo")).toHaveText(
+    "Hello Jerry! How are you today?"
+  );
+
+  // const validateText: string = await page.locator("#demo").innerText();
+  // console.log("Output text is:", validateText);
+  // await expect(page.locator("#demo")).toHaveText("You pressed Cancel!");
+
+  await page.waitForTimeout(5000);
 });
