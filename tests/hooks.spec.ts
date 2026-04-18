8eac5cba82dd5c1b4d8d16db2bfda2f7a30707a0
U-Dev <udaythakur.shely@gmail.com>
2026-04-18 09:57:21 -0700

    hooks

--- tests/hooks.spec.ts
+++ tests/hooks.spec.ts
@@ -18,14 +18,18 @@ let page: Page;
 
 test.beforeAll("Open App", async ({ browser }) => {
   page = await browser.newPage();
-  await page.goto("https://demoblaze.com/");
+  await page.goto("https://demoblaze.com/index.html");
+});
+
+test.afterAll("Close App", async () => {
+  await page.close();
 });
 
 test.beforeEach("Login", async () => {
   await page.locator("#login2").click();
-  await page.locator("loginusername").fill("pavanol");
-  await page.locator("loginpassword").fill("test@123");
-  await page.locator("button[onclick='login()']").click();
+  await page.locator("#loginusername").fill("pavanol");
+  await page.locator("#loginpassword").fill("test@123");
+  await page.locator("button[onclick='logIn()']").click();
   await page.waitForTimeout(2000);
 });
 
@@ -33,6 +37,22 @@ test.afterEach("Logout", async () => {
   await page.locator("#logout2").click();
 });
 
-test.afterAll("Close App", async () => {
-  await page.close();
+test.describe("product checkout", async () => {
+  test("Find number of products", async () => {
+    const products = page.locator("#tbodyid .hrefch");
+    const count = await products.count();
+    console.log(count);
+    await expect(products).toHaveCount(9);
+  });
+
+  test("Add product to cart", async () => {
+    await page.locator("text ='Samsung galaxy s6'").click();
+
+    // Handle alert before the click
+    page.once("dialog", async (dialog) => {
+      expect(dialog.message()).toContain("Product added");
+      await dialog.accept();
+    });
+    await page.locator(".btn.btn-success.btn-lg").click();
+  });
 });
